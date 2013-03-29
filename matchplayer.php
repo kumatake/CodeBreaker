6849ed8ff303b7d59978db7a93557e2204f8f612
walker <breveclown@gmail.com>
2013-03-29 11:16:46 +0900

    プレイヤーマッチングクラス

---
+++ matchplayer.php
@@ -0,0 +1,15 @@
+<?php
+	class matchPlayer{
+		$count;
+		$flg;
+		
+		function match(){
+			if($count<>2){
+				if(isset($_GET["startbtn"]){
+					$count++
+				}
+			}else{
+				return true;
+			}
+		}
+	}
