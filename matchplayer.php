4c11429eb2ceb91fe00ccba99488a8ead50595ae
soleclown <breveclown@gmail.com>
2013-03-29 11:33:49 +0900

    Update matchplayer.php

--- matchplayer.php
+++ matchplayer.php
@@ -1,15 +1,14 @@
 <?php
-	class matchPlayer{
-		$count;
-		$flg;
-		
-		function match(){
-			if($count<>2){
-				if(isset($_GET["startbtn"]){
-					$count++
-				}
-			}else{
-				return true;
+class matchPlayer{
+	$count;
+	
+	function match(){
+		if($count<>2){
+			if(isset($_GET["startbtn"]){
+				$count++
 			}
+		}else{
+			return true;
 		}
 	}
+}
