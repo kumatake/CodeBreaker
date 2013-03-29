6fe2e03cc9b65cdf5fdcea4c99f705ef8023ea3b
kamata <kamatuka3@gmail.com>
2013-03-29 12:07:51 +0900

    デバッグ＆修正

--- player.php
+++ player.php
@@ -1,45 +1,46 @@
 <?
+define('FIGURE_LENGTH', 3);
 
 class player
 {
 	
-	define('FIGURE_LENGTH', 3);
+	
 	
 	protected $number;	//最初に設定する数列
 	protected $history;	//履歴の配列
 	
-	public setNumber($inputNumber){
+	public function setNumber($inputNumber){
 		
-		for($i = 0; $i < FIGURE_LENGTH; i++){
+		for($i = 0; $i < FIGURE_LENGTH; $i++){
 			
-			array_push($this->number, ($inputNumber % 10));
-			$inputNumber %= 10;
+			$this->number[] = ($inputNumber % 10);
+			$inputNumber /= 10;
 		
 		}
 		
-		for($i = 0; $i < FIGURE_LENGTH / 2; i++){
+		for($i = 0; $i < FIGURE_LENGTH / 2; $i++){
 		
-			$temp = $this->number[i];
-			$this->number[i] = $this->number[FIGURE_LENGTH - 1 - i];
-			$this->number[FIGURE_LENGTH - 1 - i] = $temp;
+			$temp = $this->number[$i];
+			$this->number[$i] = $this->number[FIGURE_LENGTH - 1 - $i];
+			$this->number[FIGURE_LENGTH - 1 - $i] = $temp;
 			
 		}
 		
 	}
 	
-	public getNumber(){
+	public function getNumber(){
 		
 		return $this->number;
 		
 	}
 	
-	public addHistory($history){
+	public function addHistory($history){
 	
-		array_push($this->history, $hisroty);
+		$this->history[] = $history;
 	
 	}
 	
-	public getHistory(){
+	public function getHistory(){
 	
 		return $this->history;
 	
@@ -52,8 +53,9 @@ $player2 = new player();
 
 $player1->setNumber(192);
 $player2->setNumber(168);
-$player1->setHistory('108 2EAT 0BITE');
-$player2->setHistory('921 0EAT 3BITE');
+
+$player1->addHistory('108 2EAT 0BITE');
+$player2->addHistory('921 0EAT 3BITE');
 
 var_dump($player1->getNumber());
 var_dump($player2->getNumber());
