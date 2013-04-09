cd79340857ff6bb0a4eca0576bd3f6887c5a1a71
kamata <kamatuka3@gmail.com>
2013-04-09 11:52:42 +0900

    テスト

--- CodeBraker.php
+++ CodeBraker.php
@@ -14,17 +14,21 @@
 <div id="main">
 <p>テストなう</p>
 <?php
-echo "NUM=0";
-if($_POST['submit']){
-	echo $num;
+echo "NUM=";
+if(isset($_POST['submit'])){
+
+	$num = $_POST['text'];
 	++$num;
 	//echo $_POST['text'];
+	echo $num;
+}else{
+	$num = 0;
 	echo $num;
 }
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
 <input type="submit" name="submit" value="start">
-<input type="text" name="text">
+<input type="text" name="text" value="<? echo $num; ?>">
 </form>
 </div>
 
