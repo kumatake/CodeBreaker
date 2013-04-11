4215dc67880b5daaae11e26a63deaf8a56e80b48
walker <breveclown@gmail.com>
2013-04-11 22:14:56 +0900

    変更

--- game.php
+++ game.php
@@ -10,10 +10,9 @@ $f = file_get_contents('gameM');
 $a = unserialize($f);
 
 //処理書く
-if($_POST['set1']){
+if(isset($_POST['set1'])){
 	$a->player1set($_POST['setnum']);
-	
-	echo $_POST['setnum'];
+	//echo $_POST['setnum'];
 }elseif(isset($_POST['set2'])){
 	$a->player2set($_POST['setnum']);
 	echo '<meta http-equiv="refresh" content="0; url=./gamePlay.php">';
@@ -28,7 +27,7 @@ file_put_contents('gameM',$f);
 
 <body>
 
-<div id="pset1">
+<div id="numset">
 <p>あなたの数列を入力してください</p>
 <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
 <input type="text" name="setnum" />
--- gamePlay.php
+++ gamePlay.php
@@ -3,8 +3,34 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>コードブレイカー</title>
+<?php
+include('gameMaster.php'); 
+$f = file_get_contents('gameM');
+$a = unserialize($f);
+
+//処理書く
+if(isset($_POST['call'])){
+	$a->playGame($_POST['answer']);
+}
+$f = serialize($a);
+file_put_contents('gameM',$f);
+
+?>
 </head>
 
 <body>
+<div id="main">
+<p>相手の数列の予想を入力してください</p>
+<form action="./gamePlay.php" method="post">
+<input type="text" name="answer" />
+<input type="submit" name="call" value="コール" />
+</form>
+</div>
+<div id="his">
+<p>コール履歴</p>
+<?php
+
+ ?>
+</div>
 </body>
 </html>
