bfc14bd046c3531d9c46a1d43fdfc41d1091c50f
walker <breveclown@gmail.com>
2013-04-16 11:19:29 +0900

    桁設定変更

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ file_put_contents('gameM',$f);
 <div>
 <p>コードブレイカーを始めます</p>
 <form action="./game.php" method="post">
-<input type="number" name="setdigit" max="5" min="3" /><br>
+<input type="number" name="setdigit" max="5" min="3" value="3"/><br>
 <input type="submit" name="start" value="ゲームスタート" />
 </form>
 </div>
